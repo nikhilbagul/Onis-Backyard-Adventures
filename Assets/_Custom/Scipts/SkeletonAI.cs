36e1c9b4b79104c06bbe80c5eda56eacf507d268
Nikhil Bagul <nik2522@gmail.com>
2017-03-20 18:09:28 -0700

    -fix AI enemy pursuing animation bug

--- Assets/_Custom/Scipts/SkeletonAI.cs
+++ Assets/_Custom/Scipts/SkeletonAI.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class SkeletonAI : MonoBehaviour {
 
-    public Transform player;
+    public Transform player, enemyHead;
     public float timeToRotate;
 
     private Animator enemyAnimatorController;
     private Vector3 direction;
     private float angleFOV;
+    private bool pursuing;
 
     // Use this for initialization
     void Start ()
     {
         timeToRotate = 0.1f;
         enemyAnimatorController = GetComponent<Animator>();
+        pursuing = false;
     }
 	
 	// Update is called once per frame
@@ -23,11 +25,11 @@ public class SkeletonAI : MonoBehaviour {
     {
         direction = player.position - this.transform.position;              //calculate the direction vector from enemy to player
         direction.y = 0.0f;                                                 //prevent the enemy from bending            
-        angleFOV = Vector3.Angle(this.transform.forward, direction);
+        angleFOV = Vector3.Angle(enemyHead.transform.up, direction);
         
-        if (Vector3.Distance(this.transform.position, player.position) < 10 && angleFOV < 30)            //when the player is within enemy's range and FOV
+        if (Vector3.Distance(this.transform.position, player.position) < 10 && (angleFOV < 30 || pursuing))            //when the player is within enemy's range and FOV
         {
-            
+            pursuing = true;
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), timeToRotate);      //rotate the enemy to face the player
 
             enemyAnimatorController.SetBool("isIdle", false);
@@ -50,6 +52,7 @@ public class SkeletonAI : MonoBehaviour {
             enemyAnimatorController.SetBool("isIdle", true);
             enemyAnimatorController.SetBool("isWalking", false);
             enemyAnimatorController.SetBool("isAttacking", false);
+            pursuing = false;
         }        	
 	}
 }
