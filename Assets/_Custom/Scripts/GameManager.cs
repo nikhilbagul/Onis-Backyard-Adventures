1c7abac8afcc0a3783b714eeb9b6a0fcb093602e
nikhilbagul <nik2522@gmail.com>
2017-03-30 08:53:00 -0700

    -add menu effects

---
+++ Assets/_Custom/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Canvas gameHudCanvas;
+
+    //private Blur_MOD blurRef;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameHudCanvas.gameObject.SetActive(false);
+        Camera.main.GetComponent<Blur_MOD>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
