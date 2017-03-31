299746561aaea0abf64882f27df13b7577f29f32
nikhilbagul <nik2522@gmail.com>
2017-03-30 23:25:08 -0700

    -add main menu architecture
    -add Game Manager and Input Manager
    -add Pause function

--- Assets/_Custom/Scripts/GameManager.cs
+++ Assets/_Custom/Scripts/GameManager.cs
@@ -1,23 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     public Canvas gameHudCanvas;
+    public Button newGameButton, resumeButton, quitButton, restartButton;
 
-    //private Blur_MOD blurRef;
-
-    // Use this for initialization
+    public static bool isPaused;
+    private GameObject player;
+    
+    // initialization
     void Start()
     {
-        gameHudCanvas.gameObject.SetActive(false);
         Camera.main.GetComponent<Blur_MOD>().enabled = true;
+
+        gameHudCanvas.gameObject.SetActive(false);
+        isPaused = false;
+        resumeButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)                                     //disable user Input
+        {
+            player.GetComponent<CharacterController>().enabled = false;
+            player.GetComponent<MouseLook>().enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+               
+    }
 
+
+    public void OnNewGame()
+    {
+        newGameButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+        Camera.main.GetComponent<Blur_MOD>().enabled = false;
+        
+        if (player != null)     //enable user Input
+        {
+            player.GetComponent<CharacterController>().enabled = true;
+            player.GetComponent<MouseLook>().enabled = true;
+            gameHudCanvas.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnRestart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+
+    public void OnUnpause()
+    {
+        Camera.main.GetComponent<Blur_MOD>().enabled = false;
+        resumeButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+        //quitButton.gameObject.SetActive(false);
+
+        player.GetComponent<CharacterController>().enabled = true;
+        player.GetComponent<MouseLook>().enabled = true;
+        gameHudCanvas.gameObject.SetActive(true);
+                
+        StartCoroutine("PauseInputDelay");
+    }
+
+    public void OnPause()
+    {
+        Camera.main.GetComponent<Blur_MOD>().enabled = true;
+        resumeButton.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        //quitButton.gameObject.SetActive(true);
+
+        gameHudCanvas.gameObject.SetActive(false);
+        player.GetComponent<CharacterController>().enabled = false;
+        player.GetComponent<MouseLook>().enabled = false;
+        
+        StartCoroutine("PauseInputDelay");
+    }
+    
+    public IEnumerator PauseInputDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        isPaused = !isPaused;
     }
 }
---
+++ Assets/_Custom/Scripts/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour {
+
+
+    private GameManager gameManagerRef;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameManagerRef = this.gameObject.GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape) && !Camera.main.GetComponent<Blur_MOD>().enabled && ! GameManager.isPaused)           //check for Espace press and game pause
+        {
+            gameManagerRef.OnPause();
+        }
+
+        if (Input.GetKey(KeyCode.Escape) && Camera.main.GetComponent<Blur_MOD>().enabled && GameManager.isPaused)
+        {
+            gameManagerRef.OnUnpause();
+        }
+    }
+
+    public void OnNewGameClick()
+    {
+        gameManagerRef.OnNewGame();
+    }
+
+    public void OnRestartClick()
+    {
+        gameManagerRef.OnRestart();
+    }
+
+    public void OnQuitClick()
+    {
+        gameManagerRef.OnQuit();
+    }
+
+    public void OnResumeClick()
+    {
+        gameManagerRef.OnUnpause();    
+    }
+
+    }
